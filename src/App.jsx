56a5e9f7471499b9f5fa49f0d91cf15d1568d0de
PotatoAuthority <ahsanmanjurul27@gmail.com>
2026-03-28 23:37:16 +0600

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Countries from './Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        <h1>My React Project</h1>
+        <Countries></Countries>
+      </section>
+
+    </>
+  )
+}
+
+export default App
